a79352a718a7d5f30d38c4c0cf78cf27b59808ec
Tobias Schramm <t.schramm@manjaro.org>
2020-04-17 12:49:47 +0200

    Ensure flash images are actuall 16MB
    Temporary workaround, will eventually be fixed by
    https://github.com/pengutronix/genimage/pull/102

--- buildroot/board/pinebook_pro/post-image.sh
+++ buildroot/board/pinebook_pro/post-image.sh
@@ -24,6 +24,11 @@ $MKIMAGE -f auto -A arm64 -O linux -T kernel -C lzma -a $LOADADDR -e $LOADADDR \
 $MKIMAGE -n rk3399 -T rkspi -d $BINARIES_DIR/u-boot-tpl.bin:$BINARIES_DIR/u-boot-spl.bin \
 	 $BINARIES_DIR/idbloader-rkspi.bin
 
+# Create empty overlay image
+# Workaround for https://github.com/pengutronix/genimage/pull/102
+# Not using sparse files because of https://github.com/pengutronix/genimage/pull/106
+dd if=/dev/zero count=1 bs=2M of=$BINARIES_DIR/empty-overlay.img
+
 for devtype in sdcard nor-flash; do
 	# Build boot scripts
 	$MKIMAGE -C none -A arm64 -T script -d $BOARD_DIR/boot-${devtype}.txt\
